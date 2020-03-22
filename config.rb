143cddf2c7d511a51f031e8407ff1183a719c952
Theo Julienne <theojulienne@github.com>
2020-03-23 10:31:11 +1100

    Add state pages.

--- config.rb
+++ config.rb
@@ -16,12 +16,37 @@ page '/*.txt', layout: false
 require 'base_data'
 
 BaseData::COUNTRY_TO_ISO_NAME.each do |country_iso, country_name|
+  states = {}
+  has_state_pages = BaseData::COUNTRY_STATE_DATA.include?(country_iso)
+  if has_state_pages
+    states = BaseData::COUNTRY_STATE_DATA[country_iso]
+  end
+
   proxy "/country/#{country_iso.downcase}.html", "/country-template.html", :locals => {
     country_iso: country_iso,
     country_name: country_name,
     title: country_name,
     datasets: ["by_country/#{country_iso}"],
+    
+    has_state_pages: has_state_pages,
+    states: states,
   }
+
+  if has_state_pages
+    states.each do |state_code, state_name|
+      proxy "/country/#{country_iso.downcase}/#{state_code.downcase}.html", "/state-template.html", :locals => {
+        country_iso: country_iso,
+        country_name: country_name,
+        state_code: state_code,
+        state_name: state_name,
+        title: "#{state_name}, #{country_name}",
+        datasets: ["by_country/#{country_iso}"],
+
+        has_state_pages: has_state_pages,
+        states: states,
+      }
+    end
+  end
 end
 
 ignore '/country-template.html'
