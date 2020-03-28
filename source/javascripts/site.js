df2821a6fd2960198b93b45039a66c4529b577f7
Theo Julienne <theojulienne@github.com>
2020-03-28 12:25:14 +1100

    Add first round of mobile improvements.

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,1 +1,26 @@
-// This is where it all goes :)
+(function($) {
+  // Resize all graphs that need it
+  var resizeGraphs = function() {
+    $('.plotly-graph-needs-resize').each(function() {
+      Plotly.relayout(this.id, {});
+    });
+  };
+
+  // Toggle the side navigation
+  var toggleSidebar = function(e) {
+    $("body").toggleClass("sidebar-toggled");
+    $(".sidebar").toggleClass("toggled");
+    if ($(".sidebar").hasClass("toggled")) {
+      $('.sidebar .collapse').collapse('hide');
+    };
+    resizeGraphs();
+  };
+
+  $(document).ready(function() {
+    $("#sidebarToggleTop").on('click', toggleSidebar);
+
+    if ($(document).width() < 768) {
+      toggleSidebar();
+    }
+  });
+})(jQuery);
